688f0131da69caac6dcead3214d6c608628e0bdd
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-17 15:25:41 +0530

    added @Email & @NotBlank annotations

--- src/main/java/com/example/phoenixcodecrafter/model/User.java
+++ src/main/java/com/example/phoenixcodecrafter/model/User.java
@@ -1,6 +1,8 @@
 package com.example.phoenixcodecrafter.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -11,5 +13,8 @@ public class User {
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private int id;
         private String username;
+
+        @Email(message = "Enter valid email Id")
+        @NotBlank(message = "Email should not blank")
         private String email;
 }
