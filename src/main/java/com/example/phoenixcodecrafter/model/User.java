6e3b7f3e5f3dc6458e037fd6f2cffa23e10122f0
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-17 14:52:39 +0530

    created user entity class

---
+++ src/main/java/com/example/phoenixcodecrafter/model/User.java
@@ -0,0 +1,15 @@
+package com.example.phoenixcodecrafter.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        private String username;
+        private String email;
+}
