3524be8cf19e1ff143aaeb5546c442d444b4d28a
Rajni Rajiv Jambhale <rajnirajiv@Rajnis-MacBook-Air.local>
2026-01-17 12:44:12 +0530

    Initial SpringBoot Project setup

---
+++ src/main/java/com/example/phoenixcodecrafter/PhoenixCodeCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.example.phoenixcodecrafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixCodeCrafterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixCodeCrafterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phoenixcodecrafter/PhoenixCodeCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phoenixcodecrafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixCodeCrafterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
